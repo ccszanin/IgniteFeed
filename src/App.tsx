b1c2e34ce5dd6a396191d06e8f644baa206fc66a
Carla Cristine <carlacs.cristine@gmail.com>
2023-12-01 16:40:58 -0300

    adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Header } from './components/Header'
-import {Post} from './components/Post'
+import {Post, PostType} from './components/Post'
 import './global.css'
 import  styles from './App.module.css';
 import { Sidebar } from './components/Sidebar';
 
-const posts =[
+const posts: PostType []=[
   {
     id: 1,
     author:{
@@ -50,9 +50,7 @@ const posts =[
          return (
          <Post 
           key={post.id}
-          author={post.author}
-          content={post.content}
-          publishedAt={post.publishedAt}
+          post={post}
          />
         )
        } )}
