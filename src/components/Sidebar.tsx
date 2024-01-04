e35eaa503b85dfb565991d5b95bc5072acd7e744
Carla Cristine <carlacs.cristine@gmail.com>
2024-01-03 21:26:32 -0300

    avatar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,7 +11,7 @@ export function Sidebar(){
     />
     
     <div className={styles.profile}>
-       <Avatar src="https://scontent.fsjk1-1.fna.fbcdn.net/v/t1.18169-9/24900144_1606308282791474_6922683776110579438_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeGuaicm_qFUq5MPFEYgoHgZYrS6TYOR6fxitLpNg5Hp_MFH0PwDk2kO_YhhFe_XS86GOVghLyn4wVfflRQfwTW5&_nc_ohc=sFIbaIRqlOQAX_6hwHW&_nc_oc=AQm6ecHIZXxdn_RXJHiwuh740LFoQlFfU5wgP458rWSazh9Fglr7ywoaLY94m1vEcNFCoTKMDr8NRzXoRZv6lhrl&_nc_ht=scontent.fsjk1-1.fna&oh=00_AfDAl37vNGdmJL_URE-q0bcdxZiLVUl-NWc-9BT0IDlorg&oe=657E1C73"/>
+       <Avatar src="https://github.com/ccszanin.png"/>
        <strong>Carla Cristine</strong>
        <span>Web Developer</span>
     </div>
